d68034ebbd44be1175614151f095f1793045f511
BiancaBM <bianca.blebea91@gmail.com>
2019-02-04 21:23:35 +0200

    add tracy dev/prod

--- index.php
+++ index.php
@@ -3,8 +3,28 @@
 require ('./vendor/autoload.php');
 require_once ('./app/routes.php');
 
-
 use \Framework\Router;
+
+
+use Tracy\Debugger;
+ini_set("display_errors", 0);
+Debugger::enable(Debugger::PRODUCTION);
+if(\App\Config::ENV === 'dev') {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    Debugger::enable(Debugger::DEVELOPMENT);
+}
+//var_dump helper functions
+//bd-> dump in the debugging bar
+function bd($data) {
+    bdump($data);
+}
+//dd-> dump on the page and die
+function dd($data) {
+    dump($data);
+    die();
+}
+
 
 $router = new Router();
 
